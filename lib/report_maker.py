1a63f3a53664ec5997edccbb6c8cdd76ed58b1ff
Deokho Jeong <duckhoim@kyonggi.ac.kr>
2021-11-14 19:02:42 +0000

    :lipstick: 결과물 행 크기가 충분히 크도록 설정

--- lib/report_maker.py
+++ lib/report_maker.py
@@ -12,7 +12,7 @@ def make_report(data: List[Dict[str, Any]], report_path:str) -> str:
         "a": "답변",
         "level": "위험도"
     }
-    
+    EXT = ".xlsx"
     cur_kr_time = datetime.now(tz_KR).strftime("%y%m%d-%H%M%S")
 
     df = pd.DataFrame.from_dict(data)
@@ -20,7 +20,16 @@ def make_report(data: List[Dict[str, Any]], report_path:str) -> str:
 
     if not report_path[:-1] == "/":
         report_path += "/"
+        
+    writer = pd.ExcelWriter(report_path + cur_kr_time + EXT)
+
+    df.to_excel(writer, sheet_name='분석 결과', index=False)
     
-    df.to_excel(report_path + cur_kr_time + '.xlsx', index=False)
+    for col in df:
+        col_width = max(df[col].astype(str).map(len).max(), len(col))
+        col_idx = df.columns.get_loc(col)
+        writer.sheets['분석 결과'].set_column(col_idx, col_idx, col_width * 2)
+    
+    writer.save()
     
     return cur_kr_time
